fb38b0d2256280db5bf1a69db4f3d69cd083f321
chenmingjie_wb <2817026313@qq.com>
2020-03-06 18:05:49 +0800

    提交

--- src/main/java/cn/stylefeng/guns/modular/project/controller/TbProjectController.java
+++ src/main/java/cn/stylefeng/guns/modular/project/controller/TbProjectController.java
@@ -10,11 +10,9 @@ import cn.stylefeng.roses.core.base.controller.BaseController;
 import cn.stylefeng.roses.core.reqres.response.ResponseData;
 import cn.stylefeng.roses.core.util.ToolUtil;
 import cn.stylefeng.roses.kernel.model.exception.RequestEmptyException;
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * <p>
--- src/test/java/cn/stylefeng/guns/system/UserTest.java
+++ src/test/java/cn/stylefeng/guns/system/UserTest.java
@@ -1,10 +1,15 @@
 package cn.stylefeng.guns.system;
 
 import cn.stylefeng.guns.base.BaseJunit;
+import cn.stylefeng.guns.modular.mws.entity.AmazonConfig;
+import cn.stylefeng.guns.modular.mws.entity.Marketplace;
+import cn.stylefeng.guns.modular.mws.service.SellerService;
+import cn.stylefeng.guns.modular.mws.service.impl.SellerServiceImpl;
 import cn.stylefeng.guns.modular.system.mapper.UserMapper;
 import org.junit.Test;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 用户测试
@@ -19,7 +24,11 @@ public class UserTest extends BaseJunit {
 
     @Test
     public void userTest() throws Exception {
-
+        SellerService sellerService = new SellerServiceImpl();
+        AmazonConfig config = new AmazonConfig("AKIAEXAMPLEFWR4TJ7ZQ","amzn.mws.4ea38b7b-f563-7709-4bae-87aeaEXAMPLE", "A1IMEXAMPLEWRC", "kWcrlEXAMPLEM/LtmEENI/aVmYvHNif5zB+d9+ct");
+        config.setEndpointUrl("https://mws.amazonservices.com");
+        List<Marketplace> marketplaceList = sellerService.ListMarketplaceParticipations(config);
+        System.out.println(marketplaceList);
     }
 
 }
